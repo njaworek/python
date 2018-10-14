92168667ce11af747b5ea56b575d030a683f3be0
njaworek <43907708+njaworek@users.noreply.github.com>
2018-10-14 14:02:57 +0200

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -5,6 +5,17 @@
 def calculate(a, b, operacja):
     if operacja == "+":
         return a + b
+    elif operacja == "-":
+        return a - b
+    elif operacja == "*":
+        return a * b
+    elif operacja == "/":
+        return a / b
+
+def str_calculator(a, b, operacja):
+    if operacja == 'concat':
+        return a + b
+
 
 
 #tester@tester-m:~/spotkanie2/unitest$ cat test.py
